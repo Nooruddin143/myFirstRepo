dbd099f7a37ab15118f2aa89aa6b48d549573ff2
Nooruddin <nooruddin.mohammadi@tekschool.us>
2023-02-16 12:03:45 -0800

    Unit test class add

---
+++ src/test/unitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class unitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 15;
+		int c = a + b;
+		
+	}
+
+}
