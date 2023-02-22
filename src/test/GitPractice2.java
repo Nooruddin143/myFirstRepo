75236ec624cf9c6215b2ac7801e4ad68ed869796
Nooruddin143 <125521617+Nooruddin143@users.noreply.github.com>
2023-02-21 17:02:25 -0800

    Update GitPractice2.java
    This is for pull practice

--- src/test/GitPractice2.java
+++ src/test/GitPractice2.java
@@ -5,6 +5,7 @@ public class GitPractice2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is for push practice");
+		System.out.println("This is for pull practice");
 
 	}
 
